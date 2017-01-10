c7210f88453f434731bf49aa1ca1b94b22210608
Yandrakk <jluquear@gmail.com>
2017-01-10 14:55:06 +0100

    test  1

--- Ed_j.m
+++ Ed_j.m
@@ -2,7 +2,7 @@ clear all
 
 % Variables :
 k = physconst('Boltzman'); 
-T = 333;
+T = 300;
 W = 4.5*10^(-6);
 e = 1.6*10^(-19);
 Nd = 8*10^22; %m-3
@@ -11,7 +11,8 @@ epsilon = 12.9 ;
 syms Vh x;
 S = solve (e*Vh /(k*T) + log((Nd/Nd_plus) + 0.5*(e*Vh/(k*T))^2) == 0, Vh); %calculates potential across HLJ
 Wa = sqrt(2*epsilon*k*T/(e^2*Nd))*atan(e*S/(sqrt(2)*k*T)*sqrt(Nd_plus/Nd)); % calculates vpa with previous Vh calculated
-
+ tu lis ce message ? 
+ bonjour
 
 
 
