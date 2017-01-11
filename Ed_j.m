1f545f73f1ab3cc33172e5639ff27e371b859bd0
Yandrakk <jluquear@gmail.com>
2017-01-11 18:08:07 +0100

    Ajout absorption / flux

--- Ed_j.m
+++ Ed_j.m
@@ -7,6 +7,7 @@ W = 4.5*10^(-6); %m
 Wn_plus = 0.01e-6; %m
 Wn = 0.9e-6; %m
 e = 1.6*10^(-19);
+lambda = [0.1: 0.01 : 1] ;  % en µ frr 
 range_ndplus = [10^22 : 10^20 : 10^26];
 Nd = 2e22*ones(1,length(range_ndplus)); %m-3
 % Nd_plus = 8*10^24; %m-3
@@ -18,19 +19,23 @@ epsilon = 12.9 *8.8541878176e-12 ;
 Dp_plus = 400e-4 * k*T/e; %µ * k*T./e
 Dp = Dp_plus; % most likely we don't care about doping 
 Sp = 3e3; % m*s-1
-syms Vh x, 
-S = solve (e*Vh /(k*T) + log((Nd./range_ndplus) + 0.5*(e*Vh/(k*T))^2) == 0, Vh); %calculates potential across HLJ
-Wa = sqrt(2*epsilon*k*T./(e^2*Nd))*atan(e*S/(sqrt(2)*k*T)*sqrt(range_ndplus./Nd)); % calculates vpa with previous Vh
-%calculated NEED TO ADD SIN
+syms Vh ; 
+%S = solve (e*Vh /(k*T) + log((Nd./range_ndplus) + 0.5*(e*Vh/(k*T))^2) == 0, Vh); %calculates potential across HLJ
+%Wa = sqrt(2*epsilon*k*T./(e^2*Nd))*atan(e*S/(sqrt(2)*k*T)*sqrt(range_ndplus./Nd)); % calculates vpa with previous Vh
+
 Senn_plus = (Nd./range_ndplus) * (Dp_plus / Lp_plus) * ((Sp * Lp_plus / Dp_plus )+ tanh(Wn_plus / Lp_plus))/(1+ (Sp*Lp_plus/Dp_plus)* tanh(Wn_plus/Lp_plus));
 Sen_plus_n = (range_ndplus./Nd)*(Dp/Lp)*coth(Wn/Ln);
 Fh_1 = 1./(1+(Senn_plus./Sen_plus_n).*(range_ndplus./Nd));
 Fh_1_corrected= Fh_1 *sech(Wn/Lp);
 figure;
 semilogx(range_ndplus,Fh_1,range_ndplus,Fh_1_corrected);
-
+M = a(lambda);
+  
+Tau = f(1-0.05)*e*Lp_plus*a(lambda)/(-1+(a(lambda)*Lp_plus)^2); % R = 0.05, black material, as your soul
+Jn0_plus_x1 = Tau * (-a(lambda)*Lp_plus*exp(-a(lambda)*Wn_plus) + (((Sp*Lp_plus/Dp_plus) + a(lambda)*Lp_plus - exp(-a(lambda)*Wn_plus)*((Sp*Lp_plus*cosh(Wn_plus/Lp_plus)/Dp_plus) + sinh(Wn_plus/Lp_plus)))/((Sp*Lp_plus*sinh(Wn_plus/Lp_plus)/Dp_plus) + cosh(Wn_plus/Lp_plus))));
  
 %Creating functions 
+
 
 
 
---
+++ f.m
@@ -0,0 +1,10 @@
+function f = flux(lambda) 
+if (0.24 <= lambda ) && (lambda <= 0.48) 
+    f = 1.5*(19.7*lambda -4.7)*10^15;
+ 
+elseif (0.48 <= lambda ) && (lambda <= 1.08)
+    f = 1.5*(-2.5*lambda + 5.7)*10^15;
+end 
+
+
+end 
